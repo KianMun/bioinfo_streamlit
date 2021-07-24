c5dfd1038d435c14bfc282c548d9b733ccc6500d
Kian Mun <38237271+KianMun@users.noreply.github.com>
2021-07-24 21:42:05 +0800

    Update bioinfo_app.py

--- bioinfo_app.py
+++ bioinfo_app.py
@@ -216,7 +216,7 @@ if tool_select == "Sequence Comparison":
                 sequence_compare = seq_compare(first_sequence, second_sequence)
                 st.header("Comparison Results")
                 st.write('1st Sequence: ', sequence_compare[0])
-                st.write('              ', sequence_compare[1])
+                st.write('Comparison==> ', sequence_compare[1])
                 st.write('2nd Sequence: ', sequence_compare[2])
                 st.write('\nMatch = +, Mismatch =-, Blank(in between) = *, Trailing Blanks = _') 
                 
